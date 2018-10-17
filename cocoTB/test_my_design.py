de4636508a8e827ab8a8345991daaab914ed036e
NMikhail <Nikitin.Mikhail@mail.ru>
2018-10-18 00:23:45 +0400

    Тест на питоне с использованием cocoTB

--- cocoTB/test_my_design.py
+++ cocoTB/test_my_design.py
@@ -1,12 +1,39 @@
 import cocotb
-from cocotb.triggers import Timer
 
-@cocotb.test ()
-def my_first_test(dut):
-    dut . _log . info("Running test!")
-    for cycle in range(10):
-        dut.clk = 0
-        yield Timer(1000)
-        dut.clk = 1
-        yield Timer(1000)
-    dut._log.info("Running test!")
+from cocotb.clock import Clock
+from cocotb.triggers import Timer, RisingEdge, ReadOnly
+from cocotb.result import TestFailure, ReturnValue
+
+@cocotb.coroutine
+def data(dut, value):
+    yield RisingEdge(dut.clk)
+    dut.data = value
+    raise ReturnValue((dut.q.value))
+
+@cocotb.coroutine
+def reset_out(dut):
+    dut.reset = 1
+    yield Timer(35)
+    dut.reset = 0
+
+@cocotb.coroutine
+def mmm(dut):
+
+    yield RisingEdge(dut.clk)
+
+@cocotb.test()
+def test_ff(dut):
+    width = dut.W.value.integer
+    dut.log.info("Found FF by %d bits wide" % (width))
+    #set clocks
+    cocotb.fork(Clock(dut.clk, 10).start())
+    cocotb.fork(reset_out(dut))
+    cocotb.fork(mmm(dut))
+    for i in xrange(2**5):
+        yield data(dut, i)
+        dut.log.info("Write data: %s Read data:  %s Reset: %s" % (dut.data.value, dut.q.value, dut.reset.value))
+
+    if False:
+        dut.log.error("Error")
+        raise TestFailure
+    dut.log.info("Read data OK")
--- cocoTB/verilog_src/my_design.sv
+++ cocoTB/verilog_src/my_design.sv
@@ -1,15 +1,21 @@
-module my_design(
+`timescale 1 ns/1 ns
+module my_design
+#(parameter W = 32)
+(
     input clk,
     input reset,
-    input [7:0] data,
-    output reg [7:0] q
+    input load,
+    input [(W-1):0] data,
+    output reg [(W-1):0] q
 );
 
 always @(posedge clk) begin
     if (reset)
         q <= 0;
-    else
+    else if (load)
         q <= data;
+    else
+        q <= q +1;
 end
 
 endmodule
